30f5e23129515988c50565e2c45b285dc01544c8
Ruben Hias <ruben.hias@gmail.com>
2021-08-26 21:01:19 +0000

    Added invoice_description field on products

--- vtk_br/models/product_template.py
+++ vtk_br/models/product_template.py
@@ -4,4 +4,5 @@ from odoo import models, fields
 class ProductTemplate(models.Model):
 	_inherit = 'product.template'
 	
-	contract_text = fields.Char("Text to put on Contract")
+	contract_text = fields.Char("Contract Text")
+	invoice_description = fields.Char("Invoice Description")
