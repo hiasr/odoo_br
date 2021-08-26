680f63507723ad556f671c87b1d1ff7b6a20c02a
Ruben Hias <ruben.hias@gmail.com>
2021-08-26 19:45:29 +0000

    Added module manifest file

---
+++ vtk_br/__manifest__.py
@@ -0,0 +1,9 @@
+{
+	'name': "VTK Bedrijvenrelaties",
+	'version': "14.0.0.0",
+	'depends': ['base','sale','contacts'],
+	'author': "Ruben Hias",
+	'description': """
+		Uitbreiding van Odoo voor VTK Bedrijvenrelaties
+		""",
+}
