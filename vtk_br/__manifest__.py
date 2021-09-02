fcc74abfe3c8b9dbe2eb14d70084b9dd359b2571
Ruben Hias <ruben.hias@gmail.com>
2021-09-02 15:05:41 +0000

    :Added BR contract and Invoice report

--- vtk_br/__manifest__.py
+++ vtk_br/__manifest__.py
@@ -3,6 +3,10 @@
 	'version': "14.0.0.1",
 	'depends': ['base','sale','contacts'],
 	'author': "Ruben Hias",
+	'data': [
+		'views/reports.xml',
+		'views/templates.xml',
+	],
 	'description': """
 		Uitbreiding van Odoo voor VTK Bedrijvenrelaties
 		""",
