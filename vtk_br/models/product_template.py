497d7c1f937328ea296821058b97e90e9910080d
Ruben Hias <ruben.hias@gmail.com>
2022-07-05 14:55:56 +0200

    Update begin zomer, vanaf hier deftige commits xD

--- vtk_br/models/product_template.py
+++ vtk_br/models/product_template.py
@@ -4,7 +4,7 @@ from odoo import models, fields
 class ProductTemplate(models.Model):
 	_inherit = 'product.template'
 	
-	contract_text = fields.Text("Contract Text")
+	contract_text = fields.Text("Contract Text", translate=True)
 	invoice_description = fields.Char("Invoice Description")
 	academic_year = fields.Integer("Academic Year")
 	to_archive = fields.Boolean()
--- vtk_br/models/sale_order.py
+++ vtk_br/models/sale_order.py
@@ -6,6 +6,8 @@ class SaleOrder(models.Model):
 
 	state = fields.Selection(readonly=False)
 	contract_title = fields.Char()
+	partner_contract = fields.Boolean()
+	working_year = fields.Integer(compute='_compute_working_year')
 
 	general_discount = fields.Float(
 		string="Discount (%)",
@@ -15,10 +17,21 @@ class SaleOrder(models.Model):
 		digits="Discount",
 	)
 
+	@api.depends('create_date')
+	def _compute_working_year(self):
+		for so in self:
+			cd = so.create_date
+			if cd.month < 8:
+				so.working_year = so.year - 1
+			else:
+				so.working_year = so.year
+
+
+
 	@api.depends('amount_untaxed', 'order_line', 'order_line.product_uom_qty')
 	def _compute_general_discount(self):
 		for so in self:
-			if so.amount_untaxed >= 5000:
+			if so.amount_untaxed >= 5000 or so.partner_contract:
 				so.general_discount = 5
 				for line in so.order_line:
 					line.discount = 5
