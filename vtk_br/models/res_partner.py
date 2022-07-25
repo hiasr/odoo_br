aa2d81ad8f59bc90eaaa13b92da8267f3cbaeeb6
Ruben Hias <ruben.hias@gmail.com>
2022-07-25 21:51:27 +0200

    Added spendings field on companies

--- vtk_br/models/res_partner.py
+++ vtk_br/models/res_partner.py
@@ -1,8 +1,23 @@
-from odoo import models, fields
+from odoo import api, models, fields
+from datetime import datetime, timedelta
 
 
 class ResPartner(models.Model):
-	_inherit = "res.partner"
+    _inherit = "res.partner"
 
-	litus_username = fields.Char("Litus Username")
-	main_contact = fields.Boolean("Main Contact Person")
+    litus_username = fields.Char("Litus Username")
+    main_contact = fields.Boolean("Main Contact Person")
+    spendings = fields.Monetary(compute="_compute_spendings")
+
+    @api.depends("child_ids.sale_order_ids")
+    def _compute_spendings(self):
+        for record in self:
+            spendings = 0
+            if record.is_company:
+                for contact in record.child_ids:
+                    for so in contact.sale_order_ids:
+                        if datetime.now() < so.create_date + timedelta(
+                            days=365
+                        ) and so.state in ["done", "sale"]:
+                            spendings += so.amount_untaxed
+            record.spendings = spendings
