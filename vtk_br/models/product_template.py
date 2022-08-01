6c759198f91cf4a957c9880c6b98838adbb3b98e
Ruben Hias <ruben.hias@gmail.com>
2022-08-01 18:42:08 +0200

    Made contract_text a HTML field

--- vtk_br/models/product_template.py
+++ vtk_br/models/product_template.py
@@ -1,10 +1,11 @@
-from odoo import models, fields
+from odoo import models, fields, api
+import markdown
 
 
 class ProductTemplate(models.Model):
-	_inherit = 'product.template'
-	
-	contract_text = fields.Text("Contract Text", translate=True)
-	invoice_description = fields.Char("Invoice Description")
-	academic_year = fields.Integer("Academic Year")
-	to_archive = fields.Boolean()
+    _inherit = "product.template"
+
+    contract_text = fields.Html("Contract Text", translate=True)
+    invoice_description = fields.Char("Invoice Description")
+    academic_year = fields.Integer("Academic Year")
+    to_archive = fields.Boolean()
