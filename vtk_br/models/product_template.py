71e4c5513bd01c0373b821b64931177b61230a1b
Ruben Hias <ruben.hias@gmail.com>
2022-08-21 13:12:53 +0200

    Created extra field for contract_text translation

--- vtk_br/models/product_template.py
+++ vtk_br/models/product_template.py
@@ -5,7 +5,9 @@ import markdown
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
-    contract_text = fields.Html("Contract Text", translate=True)
+    contract_text = fields.Html("Contract Text")
+    contract_text_en = fields.Html("Contract Text English")
+
     invoice_description = fields.Char("Invoice Description")
     academic_year = fields.Integer("Academic Year")
     to_archive = fields.Boolean()
