aea0c6618621b2a7885b93d23d0438662d0d8458
Ruben Hias <ruben.hias@gmail.com>
2022-09-15 19:44:53 +0200

    Added Litus integration

--- vtk_br/__manifest__.py
+++ vtk_br/__manifest__.py
@@ -9,7 +9,7 @@
         "views/sale.xml",
         "views/product_template.xml",
         "views/res_partner.xml",
-        "actions/mailmerge.xml",
+        "actions/res_partner.xml",
     ],
     "description": """
 		Uitbreiding van Odoo voor VTK Bedrijvenrelaties
--- vtk_br/models/res_partner.py
+++ vtk_br/models/res_partner.py
@@ -2,15 +2,18 @@ from odoo import api, models, fields, modules
 import logging
 from datetime import datetime, timedelta
 import pandas as pd
+from ..lib import litus
 import os
 
 _logger = logging.getLogger(__name__)
+API_KEY = os.getenv("API_KEY")
 
 
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
     litus_username = fields.Char("Litus Username")
+    litus_id = fields.Integer("Litus ID")
 
     contact_type = fields.Selection(
         string="Contact Type",
@@ -18,6 +21,19 @@ class ResPartner(models.Model):
     )
 
     spendings = fields.Monetary(compute="_compute_spendings", store=True)
+
+    @api.model
+    def create(self, vals):
+        result = super(ResPartner, self).create(vals)
+        if result.is_company:
+            litus.create_company(result.name)
+        else:
+            if not result.email:
+                raise Warning("Contact should have an email")
+            litus.create_contact(
+                result.name, result.email, result.parent_id.get_company_id_litus()
+            )
+        return result
 
     @api.depends("child_ids.sale_order_ids")
     def _compute_spendings(self):
@@ -59,3 +75,44 @@ class ResPartner(models.Model):
             "url": f"/vtk_br/static/mailmerges/{filename}",
             "target": "new",
         }
+
+    def create_company_litus(self):
+        id = litus.create_company(self.name)
+        self.litus_id = id
+        return id
+
+    def get_company_id_litus(self):
+        if self.litus_id:
+            return self.litus_id
+
+        id = litus.get_company_id(self.name)
+        if id is not None:
+            return id
+        else:
+            return self.create_company_litus()
+
+    def create_contact_litus(self):
+        id = litus.create_company(self.name)
+        _logger.info(f"Created new contact with id {id}")
+        self.litus_id = id
+        return id
+
+    def get_contact_id_litus(self):
+        if self.litus_id:
+            return self.litus_id
+
+        company_id = self.parent_id.get_company_id_litus()
+        _logger.info(f"Company id is {company_id}")
+        return litus.create_contact(self.name, self.email, company_id)
+
+    @api.model
+    def send_activation_link(self, records):
+        for record in records:
+            if record.is_company:
+                continue
+
+            litus_id = record.get_contact_id_litus()
+            _logger.info(
+                f"Sending activation link for {record.name} with id {litus_id}"
+            )
+            litus.send_activation(litus_id)
